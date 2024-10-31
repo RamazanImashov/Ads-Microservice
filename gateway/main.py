ebb6887658b54987135bab5e7cf32ccea40e0768
RamazanImashov <ramazanimashov.dev@gmail.com>
2024-10-31 15:00:24 +0600

    edit urls

--- gateway/main.py
+++ gateway/main.py
@@ -5,16 +5,19 @@ import graphene
 import requests
 from decouple import config
 
+DRF_URL=config("DRF_URL")
+FA_URL=config("FA_URL")
+
 class Query(graphene.ObjectType):
     products = graphene.List(graphene.String)
     cart = graphene.Field(graphene.String, user_id=graphene.Int())
 
     def django_users(self, info):
-        response = requests.get(f"http://{config("DRF_URL")}:8080/products")
+        response = requests.get(f"http://{DRF_URL}:8080/products")
         return response.json()
 
     def fastapi_ads(self, info, user_id):
-        response = requests.get(f"http://{config("FA_URL")}:8001/cart/?user_id={user_id}")
+        response = requests.get(f"http://{FA_URL}:8001/cart/?user_id={user_id}")
         return response.json()
 
 app = FastAPI()
